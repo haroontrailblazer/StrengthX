6350492695a03fe34032c57d4f6b107e3d44c7d3
Haroon K M <haroonint144@gmail.com>
2025-10-10 21:53:10 +0530

    adding Breaches cout and addreses

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import zxcvbn as zac
+import hashlib
+from pwnedpasswords import pwnedpasswords as pwned
 
 # Example usage
 
 while True:
     pwd = input("Enter a password to evaluate (or 'exit' to quit): ")
+    pwdh = hashlib.sha1(pwd.encode("utf-8")).hexdigest().upper()
     if pwd.lower()=='exit':
         break
     
     # Calling the evaluation function
-    
     eval= zac.zxcvbn(pwd)
+    cout= pwned.check(pwd)
     
     # Displaying the results (result is between 0 and 4) Arguments: eval, feedback[warning, suggestions]
-    feedback = eval['feedback']
-    print(f"Password strength score (0-4): {eval['score']}")
-    print(f"Feedback: {feedback['warning']}")
-    print(f' Suggestions: {feedback["suggestions"] if feedback["suggestions"] else "None"}')
-    print(f"Estimated guesses: {eval['guesses']}")
-    print(f"Estimated crack time (seconds): {eval['crack_times_seconds']}")
+    Measures = [eval['guesses'],
+                eval['guesses_log10'],
+                eval['score'],
+                eval['calc_time'],
+                eval['crack_times_display'],
+                eval['crack_times_seconds'],
+                eval['password'],
+                eval['sequence'],
+                eval['entropy'],
+                eval['match_sequence'],
+                cout]
+    print(f"the password has been found {cout} times in data breaches.") if cout>0 else "Good news: the password isnt found in data breaches."
