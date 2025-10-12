9633fb78e66df98eeb444e238e0f5bc8e1e395fd
Haroon K M <haroonint144@gmail.com>
2025-10-12 16:31:13 +0530

    Fixes-(Charcter & Length of Password)

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
-import string
 import zxcvbn as zac
 import hashlib
+import re
 import pwinput as pn
 from pwnedpasswords import pwnedpasswords as pwned
 
 # Example usage
 
 while True:
-    # Hashing the password using SHA-1 for pwnedpasswords check
-    pwdh = hashlib.sha1(pn.pwinput("Enter a password to evaluate (or 'exit' to quit): ",mask='*').encode("utf-8")).hexdigest().upper()
-    if pwdh =='DE3AC21778E51DE199438300E1A9F816C618D33A':
+    
+    # Prompting user for password input
+    pwd = pn.pwinput("Enter a password to evaluate (or 'exit' to quit): ",mask='*')
+    if pwd.lower() =='exit':
         break
-    lower_count = upper_count = digit_count = wspace_count = special_count=0
-    for char in pwd:
-        if char in string.ascii_lowercase:
-           lower_count += 1
-        elif char in string.ascii_uppercase:
-            upper_count += 1
-        elif char in string.digits:
-            digit_count += 1
-        elif char in string.whitespace:
-            wspace_count += 1
-        
-        else:
-            special_count += 1
-        
+    
+    # Validating password complexity
+    pattern = r'^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$'
+    
+    # Regex pattern to ensure at least 8 characters, one uppercase letter, one digit, and one special character
+    if not re.match(pattern, pwd):
+        print("Password must be at least 8 characters long, contain at least one uppercase letter, one digit, and one special character.")
+        continue
+    
+    # Hashing the password using SHA-1 for pwnedpasswords check
+    pwdh = hashlib.sha1(pwd.encode("utf-8")).hexdigest().upper()
     
     # Calling the evaluation function
     eval= zac.zxcvbn(pwdh)
@@ -41,9 +39,5 @@ while True:
                 eval['sequence'],
                 eval['feedback'],
                 cout]
-    print(f"the password has been found {cout} times in data breaches.") if cout>0 else "Good news: the password isnt found in data breaches.\n")
-    print(f"The password contains {lower_count} lowercase characters.") 
-    print(f"The password contains {upper_count} uppercase characters.")
-    print(f"The password contains {digit_count} numeric characters.")
-    print(f"The password contains {wspace_count} whitespace characters.")
-    print(f"The password contains {special_count} special characters.")
+    
+    print(f"Password has been found {cout} times in data breaches. Please choose a different password.")if cout > 0 else print("Password is strong and has not been found in data breaches.")
