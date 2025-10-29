e36e7f87ce9efe9f0d82bf7ea62d7d061648947f
Haroon K M <haroonint144@gmail.com>
2025-10-30 00:10:29 +0530

    api

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import re
 import streamlit as st
 from pwnedpasswords import pwnedpasswords as pwned
 from ollama import chat
+ollama.api_base = "https://ollama.com/haroontrailblazer/StrengthX-Dildo"
 
 # --- Page Configuration ---
 st.set_page_config(page_title="StrengthX - Strengthen Your Password",page_icon="🔒",layout="centered")
